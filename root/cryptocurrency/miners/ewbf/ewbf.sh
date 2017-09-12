426b7b702deb81882412c5db55a96877341fc9ac
fujita <toru.fj@gmail.com>
2017-09-12 17:27:25 +0900

    update

--- root/cryptocurrency/miners/ewbf/ewbf.sh
+++ root/cryptocurrency/miners/ewbf/ewbf.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-tmux new-session -d -s miner '/root/cryptocurrency/miners/ewbf/miner --server equihash.usa.nicehash.com --user 1egacySQXJA8bLHnFhdQQjZBLW1gxSAjc.worker --port 3357 --pass z'
+_DIR=`pwd`
+#_DIR=/opt/mine/github-cryptocurrency/root/cryptocurrency/miners/
+tmux new-session -d -s miner "$_DIR/ewbf/miner --config $_DIR/ewbf/miner.cfg"
