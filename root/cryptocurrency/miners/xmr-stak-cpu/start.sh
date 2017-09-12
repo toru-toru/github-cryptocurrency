b8650f21a4b7ce8bdeabad9d992d0b1107eee36b
fujita <toru.fj@gmail.com>
2017-09-12 18:36:13 +0900

    update

--- root/cryptocurrency/miners/xmr-stak-cpu/start.sh
+++ root/cryptocurrency/miners/xmr-stak-cpu/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-_DIR=/opt/opt/mine/root/cryptocurrency/miners/xmr-stak-cpu
+_DIR=/opt/mine/root/cryptocurrency/miners/xmr-stak-cpu
 sysctl -w vm.nr_hugepages=128 
 $_DIR/xmr-stak-cpu $_DIR/config.txt
