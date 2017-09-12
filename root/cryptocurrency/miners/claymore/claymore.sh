33306a90506c96a3a4b68b6ca2795796701f5c06
fujita <toru.fj@gmail.com>
2017-09-12 18:58:17 +0900

    update

--- root/cryptocurrency/miners/claymore/claymore.sh
+++ root/cryptocurrency/miners/claymore/claymore.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-tmux new-session -d -s miner '/root/cryptocurrency/miners/claymore/ethdcrminer64 -epool stratum+tcp://daggerhashimoto.usa.nicehash.com:3353 -ewal 1egacySQXJA8bLHnFhdQQjZBLW1gxSAjc -epsw x -esm 3 -allpools 1 -estale 0 -dpool stratum+tcp://decred.usa.nicehash.com:3354 -dwal 1egacySQXJA8bLHnFhdQQjZBLW1gxSAjc'
+tmux new-session -d -s clay '/opt/mine/root/cryptocurrency/miners/claymore/ethdcrminer64 -epool stratum+tcp://daggerhashimoto.jp.nicehash.com:3353 -ewal 1P2gMDfpGZZgDhBedGs18g8zFq9c6FpdU1 -epsw x -esm 3 -allpools 1 -estale 0 -dpool stratum+tcp://decred.jp.nicehash.com:3354 -dwal 1P2gMDfpGZZgDhBedGs18g8zFq9c6FpdU1'
