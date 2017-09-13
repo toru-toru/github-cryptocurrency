cc87742fccae776f953757f189e1ecb6acede635
fujita <toru.fj@gmail.com>
2017-09-13 10:42:02 +0900

    update

--- root/cryptocurrency/miners/xmr-stak-cpu/start.sh
+++ root/cryptocurrency/miners/xmr-stak-cpu/start.sh
@@ -2,4 +2,6 @@
 
 _DIR=/opt/mine/root/cryptocurrency/miners/xmr-stak-cpu
 sysctl -w vm.nr_hugepages=128 
-tmux new-session -d -s xmr "$_DIR/xmr-stak-cpu $_DIR/config.txt"
+tmux new-session -d -s xmr "$_DIR/xmr-stak-cpu $_DIR/config.txt" 
+pidof xmr-stak-cpu > /var/run/xmr-stak-cpu.pid
+
