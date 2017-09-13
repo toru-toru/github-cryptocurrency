c893a14a64645af481aaef69e033588966ffa1d8
fujita <toru.fj@gmail.com>
2017-09-13 12:52:06 +0900

    update

--- root/cryptocurrency/miners/ccminer/ccminer.sh
+++ root/cryptocurrency/miners/ccminer/ccminer.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-tmux new-session -d -s miner '/root/cryptocurrency/miners/ccminer/ccminer -a lbry -o stratum+tcp://lbry.us.nicehash.com:3356 -u 1egacySQXJA8bLHnFhdQQjZBLW1gxSAjc -p x'
+tmux new-session -d -s ccminer '/opt/mine/root/cryptocurrency/miners/ccminer/ccminer -a lbry -o stratum+tcp://lbry.jp.nicehash.com:3356 -u 1P2gMDfpGZZgDhBedGs18g8zFq9c6FpdU1.1070 -p x'
+pidof ccminer > /var/run/ccminer.pid
